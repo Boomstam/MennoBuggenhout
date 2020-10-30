eb35e94b6e2485a7e0367fff8758bdc0c20b3588
Menno Buggenhout <mennobuggenhout@gmail.com>
2020-10-30 10:57:52 +0100

    InjectedJQuery

--- js/injector.js
+++ js/injector.js
@@ -2,6 +2,9 @@ const charset = "utf-8";
 const rel = "stylesheet";
 const googleFontFamilies = ["Peddana"];
 const googleFontLink = "https://fonts.googleapis.com/css?family=";
+const jQueryLink = "https://code.jquery.com/jquery-3.5.1.min.js";
+const integrity = "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=";
+const crossOrigin = "anonymous"
 
 const pages = ["Menno Buggenhout", "CV", "Projecten", "ToDo", "Evolute"];
 const globalStyles = ["global"];
@@ -13,34 +16,46 @@ const styleSheetNames = [
     []
 ];
 const scriptNames = [
-    [],
+    ["contact"],
     [],
     [],
     ["models", "helpers", "jsonManager", "collEditor", "detailHider", "modelManager", "modelParser", "main"],
     ["shapes", "actors", "clickDetection", "gameState", "gameStarter"]
 ];
 const loadDelay = 1;
+const jQueryLoadDelay = 100;
 
-injectScripts();
+inject();
 
 /* Wait time needed to ensure scripts are loaded*/
-async function injectScripts() {
-    injectHead();
+async function inject() {
+    await injectJQuery();
+    await injectNavbar();
+    injectFonts();
 
     let pageIndex = getPageIndex();
     await injectCss(pageIndex);
     await injectJs(pageIndex);
 }
 
-function getFolderPrefix(pageIndex) {
-    let folderPrefix = pages[pageIndex];
-    folderPrefix = folderPrefix + "/";
-
-    return folderPrefix;
+async function injectJQuery() {
+    let script = document.createElement('script');
+    script.src = jQueryLink;
+    script.integrity = integrity;
+    script.crossOrigin = crossOrigin;
+    document.head.appendChild(script);
+    console.log();
+    await sleep(jQueryLoadDelay);
+    /* 
+        This arbitrary delay is NOT IDEAL yet because of potentially too slow hardware/connection.
+        Make bottleneck-dependend some way or put in loop that can check if jQuery was loaded.
+    */
 }
 
-function injectHead() {
-    injectFonts();
+async function injectNavbar() {
+    $.get("nav.html", function(my_var) {
+        console.log(my_var);
+    });
 }
 
 function injectFonts() {
@@ -50,6 +65,13 @@ function injectFonts() {
         link.rel = rel;
         document.head.appendChild(link);
     }
+}
+
+function getFolderPrefix(pageIndex) {
+    let folderPrefix = pages[pageIndex];
+    folderPrefix = folderPrefix + "/";
+
+    return folderPrefix;
 }
 
 async function injectCss(pageIndex) {
@@ -77,7 +99,6 @@ function createCssLink(style) {
 async function injectJs(pageIndex) {
     let scripts = scriptNames[pageIndex];
     let prefix = getFolderPrefix(pageIndex);
-    console.log(prefix);
     for (const scrpt of scripts) {
         let source = "js/" + prefix + scrpt + ".js";
         createJsScript(source);
@@ -98,7 +119,5 @@ function sleep(ms) {
 function getPageIndex() {
     let title = document.title;
     let index = pages.indexOf(title);
-    console.log(title);
-    console.log(index);
     return index;
 }
